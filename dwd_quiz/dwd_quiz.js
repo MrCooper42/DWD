284b039d06c954c758952a456b6103dcf5a5170a
MrCooper42 <mr.cooper42co@gmail.com>
2017-02-05 16:23:54 -0700

    redoing final page

--- dwd_quiz/dwd_quiz.js
+++ dwd_quiz/dwd_quiz.js
@@ -4,7 +4,6 @@ var correctQuest = 0;
 var total;
 var selected;
 var currQuest = 0;
-var User = new Object( );
 
 ( function( ) {
   $( '#modal1' ).modal( );
@@ -14,25 +13,26 @@ var User = new Object( );
     $.getJSON( "data.json", function( result ) {
       data = result[0].questions;
       total = data.length;
-      data = shuffleArr(data);
+      data = shuffleArr( data );
       resolve( data );
     });
   });
   getData.then( function( data ) {
     nextQuestion( currQuest );
   });
-  onSubmit();
   $( '.submitAnswer' ).click( submitAnswer );
+  $( "#userForm" )[0].reset( );
 })( );
 
 function nextQuestion( currQuest ) {
   $( '.question' ).text( data[currQuest].question );
-  $( '#currentQuest' ).text( currQuest + "/" + total );
+  $( '#currentQuest' ).text( (currQuest + 1) + "/" + total );
   $( '#currScore' ).text( correctQuest + "/" + total );
+
   console.log( data, 'data' );
   total = data.length;
   var answers = data[currQuest].answers;
-  shuffleArr(answers);
+  shuffleArr( answers );
   for ( let i = 0; i < answers.length; i++ ) {
     $( '.answers' ).prepend( '<div class="hoverable answer col s12 l6" data-score=' + answers[i].score + ' id=' + i + '><p>' + answers[i].answer + '</p></div>' )
     $( '#' + i ).click($.each( function( ) {
@@ -51,9 +51,9 @@ function nextQuestion( currQuest ) {
   }
 }
 
-function shuffleArr(arr) {
-  for (var i = arr.length - 1; i > 0; i--) {
-    var j = Math.floor(Math.random() * (i + 1));
+function shuffleArr( arr ) {
+  for ( var i = arr.length - 1; i > 0; i-- ) {
+    var j = Math.floor(Math.random( ) * ( i + 1 ));
     var temp = arr[i];
     arr[i] = arr[j];
     arr[j] = temp;
@@ -67,27 +67,40 @@ function removeLast( ) {
   })
 }
 
-function onSubmit(form){
-  console.log();
-  console.log($("#userForm").serialize(), "fasdfasdf");
-  $("#userForm").reset();
-  $("#modal1").modal("close")
+function User(pic, name, played){
+  this.pic = pic;
+  this.name = name;
+  this.played = played;
+}
+
+function onSubmit( form ) {
+  var formData = $( "#userForm" ).serializeArray( )
+  var name = formData[formData.length - 1]
+  var pic = formData[0];
+  if (formData.length > 2) {
+    pic = formData[Math.floor(Math.random() * 2)];
+  }
+  var played = [];
+  var newUser = new User(pic, name, played);
+  console.log(newUser, "newUser");
+  $( "#userForm" )[0].reset( )
+  $( "#modal1" ).modal( "close" )
   return false;
 }
 
 function submitAnswer( ) {
   if ( selected.score == undefined ) {
-    removeLast( )
     Materialize.toast( 'Chose one to continue!', 3000, 'rounded' );
-  } else if ( currQuest == total ) {
-    endGame();
   } else if ( selected.score == 1 ) {
-    removeLast( );
+    correctQuest++;
     currQuest++;
   } else if ( selected.score == 0 ) {
-    removeLast( )
     currQuest++;
   }
+  if ( currQuest == total) {
+    endGame();
+  }
+  removeLast( )
   selected.score = undefined;
   nextQuestion( currQuest )
 }
@@ -95,5 +108,7 @@ function submitAnswer( ) {
 function endGame( ) {
   correctQuest = 0;
   currQuest = 0;
-  $( "#modal2" ).modal( "open" )
+  var finalScore = correctQuest + "/" + total
+  $( "#modal2" ).modal( "open" );
+  $(".congrats").append("<h1>" + finalScore + "</h1>")
 }
